cd06404f4510f0ae716b4519be5f117edf12e197
healthyMan22 <programmerjong@gmail.com>
2022-08-03 08:52:58 +0900

    test: MemberServiceTest 구현

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 public class MemoryMemberRepository implements MemberRepository{
 
-    // 콘크루트 해쉬맵에 대해서 따로 정리
-    private static Map<Long, Member> store = new HashMap<>();
+    // ConcurrentHashMap 에 대해 따로 정리
+     private static Map<Long, Member> store = new HashMap<>();
 
     @Override
     public void save(Member member) {
