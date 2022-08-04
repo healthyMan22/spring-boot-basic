95e77aa321b78551c35b2a39c650ba93fa4d2e7f
healthyMan22 <programmerjong@gmail.com>
2022-08-04 09:00:38 +0900

    feat: DIP, OCP 정리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,7 +9,24 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    /*
+        private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+        [ DIP: 추상화에 의존해야 하며, 구체화에 의존하면 안된다. ]
+
+        지금 위의 경우는 DIP 를 위반한 경우이다.
+        왜냐하면 인터페이스 뿐만 아니라 구체 클래스도 함께 의존하고 있기 떄문이다.
+
+        [ OCP : 확장에 대해서는 개방(OPEN) 되어야 하지만 변경에 대해서는 폐쇄(CLOSE) 되어야 한다.
+        -> 기존의 코드를 변경하지 않으면서 기능을 추가할 수 있다. ]
+
+        또한 할인 정책을 바꾸면 OrderServiceImpl 도 바꿔줘야 하므로 OCP 원칙에도 위반된다.
+
+        DIP, OCP 모두 SOLID 원칙 중 하나이다.
+     */
+
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
